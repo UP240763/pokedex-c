f5eae9fcef9b820f31876f687e2748d589745224
up240763 <up240763@alumnos.upa.edu.mx>
2026-03-04 15:34:25 -0600

    Show pokemon image on screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import PokemonCard from "@/components/PokemonCard";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   const [results, setResults] = useState<any[]>([]);
@@ -29,7 +30,13 @@ export default function Index() {
   return (
     <View>
       {results.map((item) => {
-        return <Text key={item.name}>{item.name}</Text>;
+        return (
+          <PokemonCard
+            key={item.name}
+            name={item.name}
+            url={item.url}
+          ></PokemonCard>
+        );
       })}
     </View>
   );
